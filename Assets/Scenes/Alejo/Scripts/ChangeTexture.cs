64b279a99e2511d1e00d1f5edafeccf13f10f161
Alejo Lopez <alejo-197@outlook.com>
2023-02-11 21:13:22 -0500

    ChangeTextures cs
    El script cambia de la texturas de varios objetos una vez el jugador entra en determinada zona (Trigger).

--- Assets/Scenes/Alejo/Scripts/ChangeTexture.cs
+++ Assets/Scenes/Alejo/Scripts/ChangeTexture.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 
 public class ChangeTexture : MonoBehaviour
 {
-    [SerializeField] Texture m_MainTexture;
-    Renderer m_Renderer;
+    /*INSTRUCCIONES: El script se agrega a un objeto padre, el cual contiene el trigger.
+     Los hijos son todos los elementos a los que se le van a cambiar la textura del material.
+    Se agregan las texturas en el inspector. Se debe QUITAR el componente MATERIAL al padre*/
+    [Header("Textures"), SerializeField] Texture [] m_OtherTexture;
+    Renderer [] m_Renderer;
+    Texture [] m_OriginalTexture;
     private void Awake() {
-        m_Renderer = GetComponent<Renderer>();
+
+        m_Renderer = GetComponentsInChildren<Renderer>();
+        m_OriginalTexture = new Texture[m_OtherTexture.Length];
     }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C)) {
-            m_Renderer.material.SetTexture("_MainTex", m_MainTexture);
+
+    private void Start() {
+
+        for(int i = 0; i <m_OtherTexture.Length; i++) { //Se guardan las texturas originales
+            m_OriginalTexture[i] = m_Renderer[i].material.mainTexture;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) { //Se cambian las texturas originales por las puestas en el inspector
+            for (int i = 0; i < m_OtherTexture.Length; i++) {
+                m_Renderer[i].material.SetTexture("_MainTex", m_OtherTexture[i]);//API Unity: SetTexture
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) { //Se regresan las texturas originales a cada G.O.
+            for(int i = 0; i < m_OtherTexture.Length; i++) {
+                m_Renderer[i].material.SetTexture("_MainTex", m_OriginalTexture[i]);
+            }
         }
     }
 }
