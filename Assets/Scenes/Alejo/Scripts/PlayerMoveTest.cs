59e8ac2e3c3b67b2b001601678a47dea1b147a49
Alejo Lopez <alejo-197@outlook.com>
2023-02-25 16:30:09 -0500

    Steps Audio
    Se agrega audio de pasos controlado desde el código de movimiento del jugador.
    Se corrige script de cambio de textura para que funcione con el URP

--- Assets/Scenes/Alejo/Scripts/PlayerMoveTest.cs
+++ Assets/Scenes/Alejo/Scripts/PlayerMoveTest.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Archivo Original de Bello, editado por: Alejo López para agregar Bobbing Walking.
+//Archivo Original de Bello, editado por: Alejo LÃ³pez para agregar Bobbing Walking.
 
 public class PlayerMoveTest : MonoBehaviour
 {
@@ -17,18 +17,23 @@ public class PlayerMoveTest : MonoBehaviour
 
     public Transform orientation;
 
-    [Header("Bobbing Walking"), SerializeField] Animator walkingAnim;
-    
     float horizontalInput;
     float verticalInput;
 
     Vector3 moveDirection;
 
     Rigidbody rb;
+    
+    //Add by Alejo:
+    [Header("Bobbing Walking"), SerializeField] Animator walkingAnim; //Para las animaciones de caminar
+    AudioSource _audioSource; // Audio pasos
+    private ushort controlPlayAudio = 0;
+    [SerializeField] private float timeToPlayAudio = 1.5f;
 
     // Start is called before the first frame update
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
@@ -37,7 +42,7 @@ public class PlayerMoveTest : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //Suelo Check °_°
+        //Suelo Check Â°_Â°
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
         MyInput();
@@ -65,14 +70,23 @@ public class PlayerMoveTest : MonoBehaviour
         //Calcular la direccion del moviemiento
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
+        
+        //****Se activan y desactivan las animaciones de Caminar**** Add by Alejo
         if(horizontalInput != 0 || verticalInput!=0) {
             walkingAnim.SetBool("isMoving", true);
+            if (controlPlayAudio == 0)
+            {
+                _audioSource.Play();
+                controlPlayAudio++;
+            }
         }
         else {
             walkingAnim.SetBool("isMoving", false);
+            _audioSource.Pause();
+            controlPlayAudio = 0;
         }
     }
+
     private void SpeedControl()
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
