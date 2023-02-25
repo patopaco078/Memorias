8974da7ea59ee04ac5e59c475659a4ce9bbe404a
Alejo Lopez <alejo-197@outlook.com>
2023-02-24 22:00:43 -0500

    Bobbing Walking V1

---
+++ Assets/Scenes/Alejo/Scripts/PlayerMoveTest.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Archivo Original de Bello, editado por: Alejo López para agregar Bobbing Walking.
+
+public class PlayerMoveTest : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    [Header("Bobbing Walking"), SerializeField] Animator walkingAnim;
+    
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Suelo Check °_°
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        //Calcular la direccion del moviemiento
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        if(horizontalInput != 0 || verticalInput!=0) {
+            walkingAnim.SetBool("isMoving", true);
+        }
+        else {
+            walkingAnim.SetBool("isMoving", false);
+        }
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        
+        //limitador de velocidad si se necesita
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+}
