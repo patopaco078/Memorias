2062c22b043d20151744ac31bebe7f8dfd1c654a
patopaco078 <fan1patopaco@gmail.com>
2023-03-03 18:10:36 -0500

    a

--- Assets/Scenes/Camargo/Scripts/MousePositionArco.cs
+++ Assets/Scenes/Camargo/Scripts/MousePositionArco.cs
@@ -7,9 +7,16 @@ public class MousePositionArco : MonoBehaviour
     [SerializeField] Camera cam;
     [SerializeField] float PositionArcoToCamera = 0.04992065f;
     [SerializeField] float positiony = 0.2428903f;
+    [SerializeField] private float speed;
+    private float lastPosition;
+
+    public float Speed { get => speed;}
 
     private void Update()
     {
+        speed = (cam.ScreenToViewportPoint(Input.mousePosition).x - lastPosition) / Time.deltaTime;
+        lastPosition = cam.ScreenToViewportPoint(Input.mousePosition).x;
+
         transform.localPosition = new Vector3((-1)*cam.ScreenToViewportPoint(Input.mousePosition).x, positiony, PositionArcoToCamera);
     }
 }
