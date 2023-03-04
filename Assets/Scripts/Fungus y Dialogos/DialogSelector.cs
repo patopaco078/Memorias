6f995ebfbe6552d5bb27621d4f4fd652c3809ba9
Nicks <1313nicov@gmail.com>
2023-03-04 07:01:04 -0500

    Fungos en la Main sin linkear

--- Assets/Scripts/Fungus y Dialogos/DialogSelector.cs
+++ Assets/Scripts/Fungus y Dialogos/DialogSelector.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 using UnityEngine.Events;
+//By Nico Villa
 
 
 public class DialogSelector: MonoBehaviour
 {
+    //Este script sirve para llamar los bloques de dialogo del fungus con los unity events
     [SerializeField] private UnityEvent doThisWhenActivate;
     public Flowchart flowTest;
     private string mensaje;
