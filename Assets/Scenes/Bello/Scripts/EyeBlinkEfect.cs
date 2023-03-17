1b158261ae2aee4830e591caddc3083a5ed324be
Sebastian Bello <sebbelgar@hotmail.com>
2023-03-16 21:42:55 -0500

    Parpadeo+flashback+cuadro
    Se implmento el parpadeo en el cuadro con la cinematica

--- Assets/Scenes/Bello/Scripts/EyeBlinkEfect.cs
+++ Assets/Scenes/Bello/Scripts/EyeBlinkEfect.cs
@@ -2,30 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EyeBlinkEfect : MonoBehaviour
 {
     [SerializeField]
     private GameObject Blink;
+    private GameObject Pj;
     [SerializeField]
     private Animator anim;
     public bool blink = false;
+    private bool isPlaying = false;
     public UnityEvent blinking;
+    public UnityEvent noBlinking;
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    private float transitionTime = 1.3f;
     // Start is called before the first frame update
     void Start()
     {
         anim = Blink.GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
+     void update()
     {
-        if (blink == true)
+        if (Input.GetKeyDown(KeyCode.P) && isPlaying)
         {
-            blinking.Invoke();
-            anim.SetTrigger("isBlinking");
+            isPlaying = false;
+            
         }
+
     }
+    // Update is called once per frame
+    public void camBlink()
+    {
+       blinking.Invoke();
+       anim.SetTrigger("isBlinking");
+        StartCoroutine(stopBlink());
+    }
+    public void BlinkExit()
+    {
+        anim.SetTrigger("isBlinking");
+    }
+    IEnumerator stopBlink ()
+    {
+        yield return new WaitForSeconds(transitionTime);
+        videoPlayer.Play();
+        isPlaying = true;
+        Invoke("BlinkExit", (float)videoPlayer.length + 0.5f - transitionTime);
+        yield return new WaitForSeconds((float)videoPlayer.length);
+        videoPlayer.Stop();
+        noBlinking.Invoke();
+        isPlaying = false;
+        
+    }
+
+    //
 
 
 }
